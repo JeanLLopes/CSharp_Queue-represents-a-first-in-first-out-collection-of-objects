1e319ad1b0061b0dcc7716c917e1617f88ac2cf9
Jean Lopes <jean.llopes@outlook.com>
2019-02-03 16:26:17 -0200

    Add methods to Queue

--- CSharp_Queue/Program.cs
+++ CSharp_Queue/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace CSharp_Queue
 {
@@ -6,7 +8,52 @@ namespace CSharp_Queue
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Start {nameof(CSharp_Queue)}");
+
+            WorkWithQueue_1();
+
+            WorkWithQueue_2();
+
+            Console.WriteLine($"Complete {nameof(CSharp_Queue)}");
+            Console.ReadKey();
+        }
+
+        private static void WorkWithQueue_2()
+        {
+            //When we want to  remove an element at the beginning of the collection and return it, we are going to use the Dequeue method:
+            Queue queueCollection2 = new Queue();
+            queueCollection2.Enqueue(54);
+            queueCollection2.Enqueue("Teste");
+            queueCollection2.Enqueue(54.10);
+
+            int number = Convert.ToInt32(queueCollection2.Dequeue());
+            Console.WriteLine($"Removed element is {number}");
+            Console.WriteLine();
+
+            foreach (var item in queueCollection2)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void WorkWithQueue_1()
+        {
+            Queue<int> intCollection = new Queue<int>();
+
+            //The Enqueue method adds an element inside a collection
+            Queue queueCollection1 = new Queue();
+            queueCollection1.Enqueue(54);
+            queueCollection1.Enqueue("Teste");
+            queueCollection1.Enqueue(54.10);
+
+            foreach (var item in queueCollection1)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
         }
     }
 }
